8f466a9bebf423753cc1930c3e4500eed81631db
Gadhiya Vrund <104295611+GadhiyaVrund@users.noreply.github.com>
2025-10-07 11:18:22 +0530

    Time: 0 ms (100%), Space: 20.5 MB (63.07%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0, far = 0,end = 0;
+        for(int i=0; i<n-1; i++){
+            far = max(far, i + nums[i]);
+            if(i == end){
+                j++;
+                end = far;
+            }
+        }
+        return j;
+    }
+};

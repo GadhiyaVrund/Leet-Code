6ed16c63a69d2f97cd98dabbb368dfef8fe86f6d
Gadhiya Vrund <104295611+GadhiyaVrund@users.noreply.github.com>
2026-02-20 17:45:58 +0530

    Time: 47 ms (64.38%), Space: 90.9 MB (44.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> hset;
-        for(int idx = 0; idx < nums.size(); idx++) {
-            if(hset.count(nums[idx]))
+        unordered_map<int,int> map;
+        for(int i : nums){
+            if(map[i] > 0){
                 return true;
-           
-            hset.insert(nums[idx]);
+            }
+            map[i]++;
         }
         return false;
     }
